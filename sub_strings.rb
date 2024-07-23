437af974fc189dd94830d53f34ee2b5f6621fd09
Marisz17 <marisaninc@gmail.com>
2024-07-23 19:26:26 +0200

    add split method to allow for multiple words

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,5 +1,8 @@
 def substrings(string, dictionary)
-  
+  string_to_array = string.downcase.split
+
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings("Below going", dictionary)

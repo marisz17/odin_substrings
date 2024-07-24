1c71b3ff76f80309eb0395b9d574650c32ba0494
Marisz17 <marisaninc@gmail.com>
2024-07-24 18:11:42 +0200

    add completed code

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,8 +1,15 @@
 def substrings(string, dictionary)
   string_to_array = string.downcase.split
 
+  dictionary.reduce(Hash.new(0)) do|return_hash, word| 
+    return_hash[word] += string_to_array.select do |string| 
+                                                string.include?(word) 
+                                                end.length
+return_hash 
+end.reject {|key, value| value == 0}
+
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-p substrings("Below going", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
